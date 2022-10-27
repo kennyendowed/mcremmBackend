911904b06f4ccec1b2a44cb1daf3ac26035bf206
kenny <kenneyg50@gmail.com>
2022-10-27 12:46:18 +0100

    kjkjkj

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app=express();
 dotenv.config();
 
 var corsOptions = {
-    origin: true,
+    origin:  '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
   // exposedHeaders: ['Authorization']
